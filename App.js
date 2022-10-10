48a21e5b50d7384c12455a566ee98852201cd812
Naveed273 <softengr273@gmail.com>
2022-10-10 05:16:01 +0500

    working on posts

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React from "react";
-import RootNavigator from "./src/navigation/RootNavigator";
-import { initializeApp } from "firebase/app";
-export default function App() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyBokhdZHgkeGTKE7u0a8av49GirjGM4NC8",
-    authDomain: "push-notifications-c03cc.firebaseapp.com",
-    projectId: "push-notifications-c03cc",
-    storageBucket: "push-notifications-c03cc.appspot.com",
-    messagingSenderId: "519165717200",
-    appId: "1:519165717200:web:f0a5d90d06459ecc658cd5",
-  };
-  initializeApp(firebaseConfig);
-  return <RootNavigator />;
-}
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
+import Message from "./src/components/Message";
+import Users from "./src/components/Users";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Message />
+          <Users />
+        </ScrollView>
+      </SafeAreaView>
+    </Provider>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 10,
+  },
+});
